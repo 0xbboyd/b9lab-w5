d2528f08493c50548679ab7df180670f988e7dbb
Brendan Boyd <firemound@gmail.com>
2016-08-05 11:48:40 -0700

    create geth v. client-sig comparison

--- truffle.js
+++ truffle.js
@@ -1,20 +1,25 @@
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "metaCoin.js": [
-      "javascripts/_vendor/angular/angular.js",
-      "javascripts/metaCoinController.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  },
-  rpc: {
-    host: "localhost",
-    port: 8545
-  }
+    build: {
+        "index.html": "index.html",
+        "client-sig.html": "client-sig.html",
+        "app.js": [
+            "javascripts/app.js"
+        ],
+        "geth-sig.js": [
+            "javascripts/geth-sig.js"
+        ],
+        "client-sig.js": [
+            "javascripts/_vendor/lightwallet/index.js",
+            "javascripts/_vendor/hooked-web3-provider/index.js",
+            "javascripts/client-sig.js"
+        ],
+        "app.css": [
+            "stylesheets/app.css"
+        ],
+        "images/": "images/"
+    },
+    rpc: {
+        host: "localhost",
+        port: 8545
+    }
 };
